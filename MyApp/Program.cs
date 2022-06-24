7d1192a6cfbbb744a058c11da0d4a19d02552d47
HelloWorld-n <gajica.b@gmail.com>
2022-06-24 17:49:39 +0200

    modified:   Program.cs

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -2,9 +2,10 @@
 using static System.Random;
 
 class Program{
+	public const String esc = "\u001B"; 
 	public static void Main(string[] args){
-		int delay = 25;
-		int delta_delay = 10;
+		int delay = 35;
+		int delta_delay = 16;
 		var rnd = new Random();
 		var now = DateTime.Now;
 		var previous = now;
@@ -23,32 +24,32 @@ class Program{
 			Console.WriteLine($"Last time updated at {now.ToString("yyyy-MM-dd'T'HH:mm:ssK")}。");
 			if (delta_now.TotalSeconds > 5){
 				Console.WriteLine(
-					"There is delay of p" + (
+					$"{esc}[38;2;127;127;127mThere is delay of P" + (
 						(
 							delta_now.TotalDays > 1
 						) ? (
-							$"{delta_now.Days}Dt"
-						) : ("t") 
+							$"{delta_now.Days}DT"
+						) : ($"T") 
 					) + (
 						(
 							delta_now.TotalHours > 1
 						) ? (
 							$"{delta_now.Hours}H"
-						) : ("") 
+						) : ($"") 
 					) +	(
 						(
 							delta_now.TotalMinutes > 1
 						) ? (
 							$"{delta_now.Minutes}M"
-						) : ("")
-					) + $"{delta_now.Seconds}S。"
+						) : ($"")
+					) + $"{delta_now.Seconds}S。{esc}[00m"
 				);
 			}
 			if (delta_now.TotalMinutes > 2.5){
 				Console.WriteLine(
-					"\u001B[48;2;0;0;0m\u001B[38;2;255;0;0m"+
-					"Warning: recommended immediate reset!"+
-					"\u001B[00m"
+					$"{esc}[48;2;0;0;0m{esc}[38;2;255;127;127m"+
+					$"Warning: recommended immediate reset!"+
+					$"{esc}[00m"
 				);
 			}
 
